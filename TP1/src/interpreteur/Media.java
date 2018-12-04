6414da5ea27f1c213e176ccfe84eec5a0da2bad1
SCHMIDT Theo <theo.schmidt@telecomnancy.eu>
2018-12-04 15:01:13 +0100

    dossier scindé marche

---
+++ TP1/src/interpreteur/Media.java
@@ -0,0 +1,12 @@
+package interpreteur;
+
+public abstract class Media {
+    protected int annee ;
+    protected String nom ;
+
+    protected Media(int a, String nom) {
+        this.annee = a;
+        this.nom = nom;
+    }
+
+}
